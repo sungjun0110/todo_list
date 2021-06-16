4b40556bb0c19ffdd859457a38ca4f50e94a9f25
sungjun0110 <55672234+sungjun0110@users.noreply.github.com>
2021-06-16 02:32:29 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React, { useState, useContext } from 'react';
 import './App.css';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Welcome from './pages/Welcome';
+
+export const CredentialsContext = React.createContext();
+
 function App() {
+  const credentialsState = useState(null);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <CredentialsContext.Provider value={credentialsState}>
+          <Switch>
+            <Route exact path="/">
+              <Welcome />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+          </Switch>
+        </CredentialsContext.Provider>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Welcome.js
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+
+import {CredentialsContext} from '../App';
+import Todos from "../components/Todo";
+
+function Welcome() {
+  const [credentials, setCredentials] = useContext(CredentialsContext);
+  const logout = () => {
+    setCredentials(null);
+  }
+
+  return (
+    <div>
+      {credentials && <button onClick={logout}>Logout</button>}
+      <h1>Welcome {credentials && credentials.username}</h1>
+      {!credentials && <Link to='/register'>Register</Link>}
+      <br />
+      {!credentials && <Link to='/login'>Login</Link>}
+      {credentials && <Todos />}
+    </div>
+  )
+}
+
+export default Welcome
